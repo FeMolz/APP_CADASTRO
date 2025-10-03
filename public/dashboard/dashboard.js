0c8892632480391fab80a67addcc47c1cc03fafe
Felipe M <felipeborbamolz@gmail.com>
2025-10-03 17:32:55 -0300

    gitignore_update

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -5,20 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!token) {
         // Se não houver token, o usuário não está logado.
         alert('Você precisa estar logado para acessar esta página.');
-        // CORRIGIDO: Adicionada a barra no início para apontar para a raiz do site
+
         window.location.href = '/login/'; 
     }
 
     // Lógica do botão de logout
     const logoutButton = document.getElementById('logout-button');
-    if (logoutButton) { // Boa prática verificar se o botão existe
+    if (logoutButton) { 
         logoutButton.addEventListener('click', () => {
             // 1. Remover o token do localStorage
             localStorage.removeItem('token');
 
             // 2. Redirecionar para a página de login
             alert('Você foi desconectado.');
-            // CORRIGIDO: Adicionada a barra no início para apontar para a raiz do site
+
             window.location.href = '/login/'; 
         });
     }
